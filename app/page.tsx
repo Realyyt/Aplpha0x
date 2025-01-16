9142ac013847c1d1b8c91f471e515c7d9be34c92
realyyt <emmyisaac60@gmail.com>
2025-01-16 18:20:54 +0100

    trash

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,25 @@
+"use client"
 import Hero from "./sections/hero";
-import ScrollHighlight from "./sections/ScrollHighlight"
+import AlphaWearableHighlight from "./sections/AlphaWearableHighlight"
+import WearableFeatureShowcase from './sections/WearableFeatureShowcase';
+import BioScanFeatures from './sections/BioScanFeatures';
+
+// ... in your page component
 
 export default function Home() {
   return (
-    <div className="scrollable">
+    <div className="scrollable overflow-x-hidden">
+      <style jsx global>{`
+        html {
+          scroll-behavior: smooth;
+        }
+      `}</style>
       <Hero/>
-      <ScrollHighlight/>
+      <AlphaWearableHighlight/>
+      <WearableFeatureShowcase 
+      watchImageUrl="/watch2.png" 
+    />
+    <BioScanFeatures />
     </div>
   );
 }
